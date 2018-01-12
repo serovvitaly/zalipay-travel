fe5280e8094f575e6ef31eae10b0a62d58fe1817
vi <serovvitaly@gmail.com>
2018-01-12 22:06:10 +0300

    Добавлена генерация sitemap.xml

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,27 @@
 |
 */
 
+Route::get('sitemap.xml', function () {
+    $documents = \App\Models\Document::where('is_active', true)->get();
+    $xml = new \DOMDocument('1.0', 'UTF-8');
+    $urlSet = $xml->createElement('urlset');
+    $urlSet->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+    /** @var \App\Models\Document $document */
+    foreach ($documents as $document) {
+        $pubDate = new \DateTime($document->created_at);
+        $loc = 'http://travel.zalipay.com/post' . $document->id;
+        $url = $xml->createElement('url');
+        $url->appendChild($xml->createElement('loc', $loc));
+        $url->appendChild($xml->createElement('lastmod', $pubDate->format('Y-m-d')));
+        $url->appendChild($xml->createElement('priority', 0.8));
+        $url->appendChild($xml->createElement('changefreq', 'monthly'));
+        $urlSet->appendChild($url);
+    }
+    $xml->appendChild($urlSet);
+    return response($xml->saveXml(), 200)
+        ->header('Content-Type', 'text/xml');
+});
+
 Route::get('/', function () {
     return view('default.index', [
         'items' => \App\Models\Document::where('is_active', 1)->paginate(10),
