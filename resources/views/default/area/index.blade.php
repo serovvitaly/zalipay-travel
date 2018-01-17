640b437fa2180fe9b4872af404b465d73b18b35c
vi <serovvitaly@gmail.com>
2018-01-17 10:27:17 +0300

    Страница местности

--- resources/views/default/area/index.blade.php
+++ resources/views/default/area/index.blade.php
@@ -19,10 +19,43 @@
         text-align: center;
         font-weight: bold;
     }
+    body {
+        background: #d7e3d9;
+    }
 </style>
 @endsection
 
 @section('content')
     <h1 class="text-image">Египет</h1>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card bg-light mb-3">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                    <h5 class="card-title">Light card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card bg-light mb-3">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                    <h5 class="card-title">Light card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-8">
+            <div class="card bg-light mb-3">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                    <h5 class="card-title">Light card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
