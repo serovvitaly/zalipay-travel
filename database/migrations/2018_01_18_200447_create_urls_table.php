25fe5ed394bc89008dafbde012e87d3d12549b4a
vi <serovvitaly@gmail.com>
2018-01-18 23:12:34 +0300

    Добавлена модель Url и миграция для неё

---
+++ database/migrations/2018_01_18_200447_create_urls_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUrlsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('urls', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('url')->unique();
+            $table->string('title');
+            $table->string('meta_title');
+            $table->string('meta_description');
+            $table->integer('http_code');
+            $table->dateTime('audited_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('urls');
+    }
+}
